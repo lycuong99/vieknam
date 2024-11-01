4d3ce5587bafe89add85b5a637d6921bcf953a82
lycuong99 <lycuong99@gmail.com>
2024-11-01 13:24:06 +0700

    feat: add model, connect db

---
+++ libs/shared-models/src/lib/_prisma.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client';
+
+export const prisma = new PrismaClient();
+export const dbTransaction = prisma.$transaction;
+export const projectModel = prisma.project;
+export const memberModel = prisma.members;
+export const userModel = prisma.user;
+export const orgModel = prisma.organization;
+export const orgMemberModel = prisma.organizationMembers;
--- libs/shared-models/src/lib/shared-types.spec.ts
+++
@@ -1,7 +0,0 @@
-import { sharedTypes } from './shared-models';
-
-describe('sharedTypes', () => {
-  it('should work', () => {
-    expect(sharedTypes()).toEqual('shared-models');
-  });
-});

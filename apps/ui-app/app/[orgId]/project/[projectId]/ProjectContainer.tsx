f443c477aabae2b0db1da95049424c78da43d998
lycuong99 <lycuong99@gmail.com>
2024-11-18 14:28:44 +0700

    feat: update project component

--- apps/ui-app/app/[orgId]/project/[projectId]/ProjectContainer.tsx
+++ apps/ui-app/app/[orgId]/project/[projectId]/ProjectContainer.tsx
@@ -2,29 +2,36 @@
 
 import { useProjectStore } from 'apps/ui-app/store/project';
 import { Calendar1Icon, Cog, UserCircle2Icon, ViewIcon } from 'lucide-react';
+import { useParams, useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { HiOutlineMenuAlt1 } from 'react-icons/hi';
+import { HiOutlineCog6Tooth, HiOutlineUserCircle, HiOutlineViewColumns, HiOutlineCalendar } from 'react-icons/hi2';
 
 export default function ProjectContainer() {
 	const { selectedProject } = useProjectStore(state => state);
 
 	const [tabs] = useState([
-		{ name: 'Overview', href: '#', icon: UserCircle2Icon, current: false },
-		{ name: 'Board', href: '#', icon: ViewIcon, current: false },
-		{ name: 'Calendar', href: '#', icon: Calendar1Icon, current: false },
-		{ name: 'Setting', href: '#', icon: Cog, current: false }
+		{ name: 'Overview', href: '#', icon: HiOutlineUserCircle, current: false },
+		{ name: 'Task', href: '#', icon: HiOutlineMenuAlt1, current: false },
+		{ name: 'Board', href: '#', icon: HiOutlineViewColumns, current: false },
+		{ name: 'Calendar', href: '#', icon: HiOutlineCalendar, current: false },
+		{ name: 'Setting', href: '#', icon: HiOutlineCog6Tooth, current: false }
 	]);
+	const params = useParams();
+	const { push } = useRouter();
 
 	const mode = 'overview';
 
 	const onMoveTab = (name: string) => {
 		console.log(name);
+		push(`/${params.orgId}/project/${params.projectId}?mode=${name.toLowerCase()}`);
 	};
 
 	return (
 		<div>
 			<div className="bg-white border-b border-gray-100 shadow-sm">
 				<h2 className="text-xl font-bold px-4 py-2">{selectedProject?.name}</h2>
-				<div className="tab pl-1">
+				<div className="tab pl-1 flex">
 					{tabs.map((tab, index) => {
 						const Icon = tab.icon;
 						const active = tab.name.toLowerCase() === mode;
